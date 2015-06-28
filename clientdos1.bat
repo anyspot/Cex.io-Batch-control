2c6e0f1a62b24fb27e7e33ef0be0ac9a2600d696
anyspot <anyspottv@gmail.com>
2015-06-29 00:32:50 +0200

    Create clientdos1.bat

---
+++ clientdos1.bat
@@ -0,0 +1,4 @@
+@echo off
+title Cex.io Launcher
+MSG * Hello %username% you cannot youse this tool yet. Please Download the latest version of my Tool at itsdoesnotexist.weebly.com %time% %date%
+exit
